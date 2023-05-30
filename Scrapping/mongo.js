be7259e0b367d4f25a6dec9719665a9457f9b36a
Javier Flores <jffc.dev@gmail.com>
2023-05-29 23:03:19 -0500

    feat(App): mongo file base added

---
+++ Scrapping/mongo.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+
+// Configura la URL de conexión de MongoDB
+const url = 'mongodb://localhost:27017';
+const dbName = 'mydatabase';
+
+// Crea un nuevo cliente de MongoDB
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Conecta con el servidor de MongoDB
+client.connect((err) => {
+  if (err) {
+    console.error('Error al conectar con MongoDB:', err);
+    return;
+  }
+
+  console.log('Conexión exitosa con MongoDB');
+
+  // Selecciona la base de datos
+  const db = client.db(dbName);
+
+  // Aquí puedes realizar operaciones con la base de datos
+
+  // Cierra la conexión al finalizar
+  client.close();
+});
