f1f6f2dc05b9f5bb7b0ea12f586bd80d396734ef
Javier Flores <jffc.dev@gmail.com>
2023-06-07 02:01:42 -0500

    feat(Scrap): mongo repo added

--- Scrapping/mongo.js
+++ Scrapping/mongo.js
@@ -1,17 +1,23 @@
-import { MongoClient } from 'mongodb'
+import pckMongo from 'mongodb'
+
+const { MongoClient } = pckMongo
 
 // Configura la URL de conexión de MongoDB
-const url = 'mongodb://localhost:27017';
-const dbName = 'mydatabase';
+const url = 'mongodb://127.0.0.1:27017/';
+const dbName = 'CulturaFactos';
 
-// Crea un nuevo cliente de MongoDB
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(url, {
+  useUnifiedTopology: true
+});
 
 // Función para conectar a la base de datos
-function conectarDB() {
+const conectarDB = () => {
+  // Crea un nuevo cliente de MongoDB
   return new Promise((resolve, reject) => {
     // Conecta con el servidor de MongoDB
+    console.log("asdasd");
     client.connect((err) => {
+      console.log("asdasd1");
       if (err) {
         reject(err);
         return;
@@ -29,24 +35,41 @@ function conectarDB() {
 }
 
 // Función para listar los documentos de una colección
-async function list(coleccion) {
+export const list = async(coleccion) => {
   try {
     // Conecta a la base de datos
     const db = await conectarDB();
 
     // Obtiene la referencia a la colección
     const collection = db.collection(coleccion);
-
-    // Realiza la consulta para obtener los documentos
     const documentos = await collection.find().toArray();
 
     // Cierra la conexión al finalizar
     client.close();
-
-    // Retorna los documentos encontrados
     return documentos;
+
   } catch (error) {
     console.error('Error al listar los documentos:', error);
+    throw error;
+  }
+}
+
+// Función para insertar un documento en una colección
+export const createOne = async(coleccion, documento) => {
+  try {
+    // Conecta a la base de datos
+    const db = await conectarDB();
+
+    // Obtiene la referencia a la colección
+    const collection = db.collection(coleccion);
+    const resultado = await collection.insertOne(documento);
+
+    // Cierra la conexión al finalizar
+    client.close();
+    return resultado;
+
+  } catch (error) {
+    console.error('Error al insertar el documento:', error);
     throw error;
   }
 }
