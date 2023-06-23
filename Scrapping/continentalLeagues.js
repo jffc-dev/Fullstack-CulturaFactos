dfa6f24ff6728b2cf3579a23fc0628b98bb04294
Javier Flores <jffc.dev@gmail.com>
2023-06-23 06:42:45 -0500

    feat(Scrap): scrap for cups added

--- Scrapping/continentalLeagues.js
+++ Scrapping/continentalLeagues.js
@@ -3,6 +3,13 @@ import { readDBFileOrCreate, scrape, writeDBFile } from "./utils.js";
 export const scrapLeagues = async(urlLeagues, urlLeagueBase, urlBase) => {
     const fileName = 'leagues'
     const leaguesArray = await readDBFileOrCreate(fileName,'json',[])
+
+    const uniqueTypes = leaguesArray.reduce((types, item) => {
+        types.add(item.type);
+        return types;
+      }, new Set());
+      
+      console.log([...uniqueTypes]);
     for (const leagueUrl of urlLeagues) {
         const urlLeague = urlLeagueBase.replace('#LEAGUE#', leagueUrl)
 
@@ -29,12 +36,51 @@ export const scrapLeagues = async(urlLeagues, urlLeagueBase, urlBase) => {
                         'shortname': leagueName,
                         'fullname': leagueFullname,
                         'image': leagueImage,
-                        'country': leagueCountry
+                        'country': leagueCountry,
+                        'type': 'national'
                     })
                     writeDBFile(fileName,leaguesArray)
                 }
             }
         }
+
+        const cups = $('main div.row div:nth-child(2) div.box a')
+
+        for (let i = 0; i < cups.length; i++) {
+            const $el = $(cups[i])
+            const cupShortname = $el.attr('title')
+            const cupUrl = $el.attr('href')
+
+            const validation = leaguesArray.find(league => league.link === cupUrl)
+
+            if(!validation){
+                const $urlDetail = await scrape(urlBase+cupUrl);
+                const cupFullname = $urlDetail('h1.data-header__headline-wrapper').text().trim();
+                const cupImage = $urlDetail('.data-header__profile-container img').attr('src');
+                const cupCountry = $urlDetail('.data-header__box--big .data-header__club a').text().trim();
+                const cupLabels = $urlDetail('li.data-header__label');
+                let cupType = null
+
+                for (let j = 0; j < cupLabels.length; j++) {
+                    const $labels = $(cupLabels[j])
+                    const text = $labels.text().trim().split('\n');
+                    if(text[0] === 'Tipo de copa:'){
+                        cupType = text[2].trim()
+                    }
+                }
+
+                leaguesArray.push({
+                    'link': cupUrl,
+                    'shortname': cupShortname,
+                    'fullname': cupFullname,
+                    'image': cupImage,
+                    'country': cupCountry,
+                    'type': cupType
+                })
+                writeDBFile(fileName,leaguesArray)
+            }
+        }
     }
     return leaguesArray;
 }
+
