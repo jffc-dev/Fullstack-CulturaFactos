25319c6921af3d73b12f55fc40141bd34042903d
Javier Flores <jffc.dev@gmail.com>
2023-07-11 22:20:55 -0500

    feat(Scrap): index file updated

--- Scrapping/index.js
+++ Scrapping/index.js
@@ -1,23 +1,37 @@
 import * as dotenv from 'dotenv'
-import { getFullInfoPlayers, getUniqueInfo } from './players.js'
-import { getFullInfoTeams } from './teams.js'
+import { getFullInfoPlayers, getPlayersInfoPiece, getUniqueInfo } from './players.js'
+import { getNameTeams, getUniqueTeams, writeInfoTeamsFile } from './teams.js'
 import { createMultiplePlayers } from './mongo/player.js'
 import { readDBFile } from './utils.js'
+import { cleanSeasonsByLeague, cleanTeamsLinksInSeasonsByLeague, scrapLeagues } from './continentalLeagues.js'
+import { STEP_1_CLEANINVALIDTEAMS, STEP_2_CLEANSEASONTEAMS } from './utils/consts.js'
 
 dotenv.config()
 
 const urlBase = process.env.URL_BASE
 const urlLeague = process.env.URL_EREDIVISIE
 
-// await getFullInfoTeams(urlBase, urlLeague, 'teams_eredivisie')
+const urlLeagues = JSON.parse(process.env.URL_CONTINENTAL_LEAGUES.replace(/'/g, '"'))
+const urlLeagueBase = process.env.URL_CONTINENTAL_LEAGUE_BASE
+
+// const array = await readDBFile('seasonsByLeague')
+// console.log(array.length);
+// await cleanTeamsLinksInSeasonsByLeague(true,STEP_2_CLEANSEASONTEAMS,false)
+await scrapLeagues(urlLeagues, urlLeagueBase, urlBase);
+// await cleanSeasonsByLeague()
+
+// await getNameTeams(urlBase, urlLeague, 'teams_eredivisie')
 // await getFullInfoPlayers(urlBase, 'teams_eredivisie',[])
 // await getUniqueInfo(['players_teams_league_1','players_teams_eredivisie'])
 
-const migratePlayersToDb = async() => {
-    const arrayPlayers = await readDBFile('/playersByTeam/players_teams_bundesliga')
-    console.log(arrayPlayers);
-    const response = await createMultiplePlayers(arrayPlayers)
-    console.log(response);
-}
+// const migratePlayersToDb = async() => {
+//     const arrayPlayers = await readDBFile('/playersByTeam/players_teams_bundesliga')
+//     console.log(arrayPlayers);
+//     const response = await createMultiplePlayers(arrayPlayers)
+//     console.log(response);
+// }
 
-migratePlayersToDb()
+// migratePlayersToDb()
+
+// await writeInfoTeamsFile(urlBase)
+// await getPlayersInfoPiece(1,"asdfasdf")
