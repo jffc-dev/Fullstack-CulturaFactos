0a52328723ea4f03d9ee2f8a7427d5c39313d696
Javier Flores <jffc.dev@gmail.com>
2023-07-14 05:35:20 -0500

    feat(Admin-Back): ONDType object added

---
+++ Administrator/Servicio/Models/General/ONDType.cs
@@ -0,0 +1,19 @@
+﻿namespace Servicio.Models.General
+{
+    public class ONDType
+    {
+        #region Miembros
+        public string TableCode { get; set; }
+        public string TypeCode { get; set; }
+        public string Description1 { get; set; }
+        public string CreationUser { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string ModificationUser { get; set; }
+        public DateTime? ModificationDate { get; set; }
+        #endregion
+
+        #region Constructor
+        public ONDType() { }
+        #endregion
+    }
+}
