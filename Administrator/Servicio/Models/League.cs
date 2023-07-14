f43a53eb419747d0a624638fbe19507d18a1c0fc
Javier Flores <jffc.dev@gmail.com>
2023-07-14 05:22:21 -0500

    feat(Admin): mongo driver package installed

---
+++ Administrator/Servicio/Models/League.cs
@@ -0,0 +1,11 @@
+﻿namespace Servicio.Models
+{
+    public class League
+    {
+        public int Id { get; set; }
+        public string Link { get; set; }
+        public string Shortname { get; set; }
+        public string Fullname { get; set; }
+        public string Image { get; set; }
+    }
+}
