528bf29707d68945c4cda7b04a0f1f4ef7ddb827
Javier Flores <jffc.dev@gmail.com>
2023-07-19 07:44:43 -0500

    feat(Admin): solution files reorganized

--- Administrator/Servicio/Program.cs
+++ Administrator/Servicio/Program.cs
@@ -1,11 +1,17 @@
+using Servicio.Models;
+using Servicio.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<CulturaFactosDataBaseSettings>(
+    builder.Configuration.GetSection("CulturaFactosDatabase"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<TypesService>();
 
 var app = builder.Build();
 
