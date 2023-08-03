dfde3bd11bbd6de360f306a081516d08d9aa9fc5
Javier Flores <jffc.dev@gmail.com>
2023-08-02 23:35:57 -0500

    feat(Scrap): getByUsername function added

--- Scrapping/mongo/user.js
+++ Scrapping/mongo/user.js
@@ -1,8 +1,14 @@
-import { createOne } from "../mongo.js";
+import { createOne, get } from "../mongo.js";
 
 const COLLECTION_NAME = 'user';
 
 export const createUser = async(player) => {
     const result = await createOne(COLLECTION_NAME, player);
     return result
+}
+
+export const getByUsername = async(username) => {
+    const query = {Username: username}
+    const players = await get(COLLECTION_NAME, query);
+    return players[0]
 }
