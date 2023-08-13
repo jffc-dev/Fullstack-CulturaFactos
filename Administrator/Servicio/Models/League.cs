719e9247bf682a6aabe2057e1abca8cdd59bbf47
Javier Flores <jffc.dev@gmail.com>
2023-08-12 23:05:12 -0500

    feat(Admin-Back): League model added

--- Administrator/Servicio/Models/League.cs
+++ Administrator/Servicio/Models/League.cs
@@ -2,10 +2,16 @@
 {
     public class League
     {
+        #region Miembros
         public int Id { get; set; }
         public string Link { get; set; }
         public string Shortname { get; set; }
         public string Fullname { get; set; }
         public string Image { get; set; }
+        #endregion
+
+        #region Constructor
+        public League() { }
+        #endregion
     }
 }
