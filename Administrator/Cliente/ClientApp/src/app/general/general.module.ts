7ac380b150f48f533b9e3a0b07b7b3a5d4ac1e7f
Javier Flores <jffc.dev@gmail.com>
2023-08-16 23:21:39 -0500

    feat(Admin-Front): general modula added

---
+++ Administrator/Cliente/ClientApp/src/app/general/general.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { FormTypeComponent } from './components/maintenance/type/formType.component';
+import { FormTypeModalComponent } from './components/maintenance/type/modal/formTypeModal.component';
+import { CounterComponent } from './components/navigation/counter/counter.component';
+import { FetchDataComponent } from './components/navigation/fetch-data/fetch-data.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { TagModule } from 'primeng/tag';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+
+@NgModule({
+  declarations: [
+    FormTypeComponent,
+    FormTypeModalComponent,
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: FormTypeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+    ]),
+    BrowserAnimationsModule,
+    TableModule,
+    ToastModule,
+    TagModule,
+    ConfirmDialogModule,
+    DialogModule,
+    InputTextModule,
+    ToolbarModule,
+    ButtonModule,
+    DropdownModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class GeneralModule { }
