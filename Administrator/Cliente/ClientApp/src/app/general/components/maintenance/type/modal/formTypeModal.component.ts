7bf2a3641708c3ae71eb26fd6043221b4aa1711b
Javier Flores <jffc.dev@gmail.com>
2023-08-24 00:57:55 -0500

    feat(Admin-Front): FormTypeModalComponent formgroup improved

--- Administrator/Cliente/ClientApp/src/app/general/components/maintenance/type/modal/formTypeModal.component.ts
+++ Administrator/Cliente/ClientApp/src/app/general/components/maintenance/type/modal/formTypeModal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Type } from '../../../../domain/type';
 import { FormControl, FormGroup } from '@angular/forms';
+import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { TYPE_OPERATION_UPDATE } from '../../../../../../constants/general/general.constants';
 
 @Component({
   selector: 'app-form-type-modal',
@@ -12,11 +14,24 @@ export class FormTypeModalComponent implements OnInit {
   formGroup!: FormGroup;
   submitted: boolean = false;
   type!: Type;
+  typesSelect!: Type[];
+  typeOperation: String = "";
+
+  constructor(public ref: DynamicDialogRef, public config: DynamicDialogConfig) { }
 
   ngOnInit() {
     this.formGroup = new FormGroup({
-      text: new FormControl<string | null>(null)
+      tableCode: new FormControl<string | null>(null),
+      typeCode: new FormControl<string | null>(null),
+      description1: new FormControl<string | null>(null),
+      description2: new FormControl<string | null>(null)
     });
+
+    this.typeOperation = this.config.data.typeOperation;
+
+    if (this.typeOperation === TYPE_OPERATION_UPDATE) {
+      this.formGroup.get('typeCode')!.disable();
+    }
   }
 
   saveProduct() {
