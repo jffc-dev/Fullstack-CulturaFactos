1e419a0af75a4d919317820877271cf16f191fe8
Javier Flores <jffc.dev@gmail.com>
2023-08-26 22:31:46 -0500

    feat(Admin-Front): app module file added

--- Administrator/Cliente/ClientApp/src/app/app.module.ts
+++ Administrator/Cliente/ClientApp/src/app/app.module.ts
@@ -3,49 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './general/components/navigation/nav-menu/nav-menu.component';
+import { GeneralModule } from './general/general.module';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TableModule } from 'primeng/table';
-import { ToastModule } from 'primeng/toast';
-import { TagModule } from 'primeng/tag';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DialogModule } from 'primeng/dialog';
-import { InputTextModule } from 'primeng/inputtext';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    NavMenuComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+
     ]),
-    BrowserAnimationsModule,
-    TableModule,
-    ToastModule,
-    TagModule,
-    ConfirmDialogModule,
-    DialogModule,
-    InputTextModule,
-    ToolbarModule,
-    ButtonModule
+    GeneralModule
   ],
   providers: [],
   bootstrap: [AppComponent]
