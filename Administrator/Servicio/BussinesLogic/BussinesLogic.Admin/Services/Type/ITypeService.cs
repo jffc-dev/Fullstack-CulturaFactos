2e999bdc845ff9049dd97ab4b48a426f75cd6949
Javier Flores <jffc.dev@gmail.com>
2023-08-27 21:01:16 -0500

    feat(Admin-Back): ITypeService file added

---
+++ Administrator/Servicio/BussinesLogic/BussinesLogic.Admin/Services/Type/ITypeService.cs
@@ -0,0 +1,20 @@
+﻿using Servicio.Models.General;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLogic.Admin.Services
+{
+    public interface ITypeService
+    {
+        public Task<ONDType> CreateAsync(ONDType newBook);
+
+        public Task<ONDType?> GetAsync(string id);
+
+        public Task UpdateAsync(string id, ONDType updatedBook);
+
+        public Task RemoveAsync(string id);
+    }
+}
