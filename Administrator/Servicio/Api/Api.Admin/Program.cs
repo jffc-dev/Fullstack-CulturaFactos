3fb2b192aa99611dee5131b7168d9fe999d5a260
Javier Flores <jffc.dev@gmail.com>
2023-08-30 22:03:50 -0500

    feat(Admin-Back): cors configuration added

---
+++ Administrator/Servicio/Api/Api.Admin/Program.cs
@@ -0,0 +1,46 @@
+using BussinesLogic.Admin.Services;
+using Servicio.Models;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("*")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+// Add services to the container.
+builder.Services.Configure<CulturaFactosDataBaseSettings>(
+    builder.Configuration.GetSection("CulturaFactosDatabase"));
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<TypeService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
