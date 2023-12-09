064cfaa469657b922621de9387f418ebb7501f5a
Javier Flores <jffc.dev@gmail.com>
2023-12-08 23:31:32 -0500

    feat: modelToOND, toModel and toOND functions added to ONDUser

--- Scrapping/models/ONDUser.js
+++ Scrapping/models/ONDUser.js
@@ -1,3 +1,6 @@
+import pckMongo from 'mongodb'
+const { ObjectId } = pckMongo
+
 export const ONDUser = class {
     constructor ({Username, Passsword, Fullname, CreationUser, CreationDate, ModificationUser, ModificationDate}) {
       this.Username = Username
@@ -7,5 +10,41 @@ export const ONDUser = class {
       this.CreationDate = CreationDate
       this.ModificationUser = ModificationUser
       this.ModificationDate = ModificationDate
+    }
+
+    modelToOND (model){
+        this.Id = new ObjectId(model._id)
+        this.Username = model.Username
+        this.Passsword = model.Passsword
+        this.Fullname = model.Fullname
+        this.CreationUser = model.CreationUser
+        this.CreationDate = model.CreationDate
+        this.ModificationUser = model.ModificationUser
+        this.ModificationDate = model.ModificationDate
+    }
+
+    toModel (){
+        return {
+            Username: this.Username
+            ,Passsword: this.Passsword
+            ,Fullname: this.Fullname
+            ,CreationUser: this.CreationUser
+            ,CreationDate: this.CreationDate
+            ,ModificationUser: this.ModificationUser
+            ,ModificationDate: this.ModificationDate
+        }
+    }
+
+    toOND (){
+        return {
+            Id: this.Id
+            ,Username: this.Username
+            ,Passsword: this.Passsword
+            ,Fullname: this.Fullname
+            ,CreationUser: this.CreationUser
+            ,CreationDate: this.CreationDate
+            ,ModificationUser: this.ModificationUser
+            ,ModificationDate: this.ModificationDate
+        }
     }
 }
